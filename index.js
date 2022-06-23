462f922b6557cb7830a0a56166b95ba7bb654a43
Nadhir <nadhirxz@gmail.com>
2022-06-23 12:36:19 +0100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,24 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+socket.on('chat message', function(msg){
+    socket.on('sender', function(uid){
+      console.log(uid + ': ' + msg);
+      io.emit('chat message', msg);
+    });
+  });
+});
